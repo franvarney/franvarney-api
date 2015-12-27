7f35d5133ece8df1e4d948cc24e9a4b5ca27e61d
Fran Varney <fran@modulus.io>
2015-12-27 03:07:44 -0500

    Add mongo connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 import {Server} from 'hapi'
 import Logger from '@modulus/logger'
+import Mongoose from 'mongoose'
 
 import Config from '../config'
 import Routes from './routes'
 
 let logger = Logger('server/index')
 let server = new Server()
+
+Mongoose.connect(Config.mongo.url, (err) => {
+  if (err) {
+    logger.error(`Mongoose.connect error: ${err.message}`)
+    throw err
+  }
+
+  logger.info(`Connected to ${Config.mongo.url}`)
+})
 
 server.connection({
   host: Config.env !== 'production' ? Config.host : null,
