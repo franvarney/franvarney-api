7584415a880a202c06ef4f49a18aa2415453e958
Fran Varney <fran@modulus.io>
2015-12-27 03:07:27 -0500

    Add mongo url option

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 export default {
   env: process.env.NODE_ENV,
   host: process.env.HOST || 'localhost',
+  mongo: {
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017/FranVarney'
+  },
   port: process.env.PORT || 8000
 }
