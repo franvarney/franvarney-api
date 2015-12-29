0d796d810a593f771745b474a64c3faa8bf5a645
Fran Varney <fran@modulus.io>
2015-12-29 00:33:34 -0500

    Add auth handler tests

---
+++ test/handlers/auth.js
@@ -0,0 +1,37 @@
+import {expect} from 'code'
+import Lab from 'lab'
+import {Server} from 'hapi'
+
+const lab = exports.lab = Lab.script()
+const {describe, it, before, after, beforeEach, afterEach} = lab
+
+import Auth from '../../server/handlers/auth'
+import Config from '../../config'
+
+describe('handlers/auth', () => {
+  describe('validate', () => {
+    describe('when a token is valid', () => {
+      it('returns valid with the token', (done) => {
+        Auth.validate('secret', (err, isValid, token) => {
+          expect(err).to.be.null()
+          expect(isValid).to.be.true()
+          expect(token.token).to.equal(Config.authToken)
+
+          done()
+        })
+      })
+    })
+
+    describe('when a token is not valid', () => {
+      it('returns invalid with the token', (done) => {
+        Auth.validate('secret2', (err, isValid, token) => {
+          expect(err.message).to.equal('Invalid token')
+          expect(isValid).to.be.false()
+          expect(token.token).to.equal('secret2')
+
+          done()
+        })
+      })
+    })
+  })
+})
