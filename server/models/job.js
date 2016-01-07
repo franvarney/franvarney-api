6b0ad568d8c1750c2f3758c1950e742e6b36e820
Fran Varney <fran@modulus.io>
2016-01-07 02:32:18 -0500

    Add pre save check

--- server/models/job.js
+++ server/models/job.js
@@ -19,4 +19,18 @@ let Job = new Schema({
 
 Job.plugin(Timestamps)
 
+Job.pre('save', function (next) {
+  Mongoose.models['Job'].find({
+    employer: this.employer,
+    dates: {
+      start: this.dates.start,
+      end: this.dates.end
+    }
+  }, (err, results) => {
+    if (err) return next(new Error(err.message))
+    if (results > 0) return next(new Error('Job already exists'))
+    next()
+  })
+})
+
 export default Mongoose.model('Job', Job)
