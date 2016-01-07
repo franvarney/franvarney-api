e1ede86835153545f262f08e039bbdb471f53ecc
Fran Varney <fran@modulus.io>
2016-01-07 15:39:37 -0500

    Add github api and username options

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ export default {
   authToken: process.env.AUTH_TOKEN || 'secret',
   env: process.env.NODE_ENV,
   host: process.env.HOST || 'localhost',
+  github: {
+    apiUrl: 'https://api.github.com',
+    username: process.env.GITHUB_USERNAME || 'username'
+  },
   mongo: {
     url: process.env.MONGO_URL || 'mongodb://localhost:27017/FranVarney'
   },
