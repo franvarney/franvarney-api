4b3f5e2503f73c94db8325905b686835a83bbac6
Fran Varney <fran@modulus.io>
2016-01-07 23:20:43 -0500

    Add github activity model

---
+++ server/models/github/activity.js
@@ -0,0 +1,13 @@
+import Mongoose, {Schema} from 'mongoose'
+import Timestamps from 'mongoose-timestamp'
+
+let GithubActivity = new Schema({
+  id: { type: String, required: true, index: true },
+  type: { type: String, required: true },
+  count: { type: String, default: 1 },
+  created: { type: String, required: true }
+})
+
+GithubActivity.plugin(Timestamps)
+
+export default Mongoose.model('GithubActivity', GithubActivity)
