8534dad79790b6a681df6cb0b4d36d72997de57c
Fran Varney <fran@modulus.io>
2016-01-10 03:51:37 -0500

    Add job to update a cache of total activities per day

---
+++ server/jobs/update-cache.js
@@ -0,0 +1,50 @@
+import Logger from '@modulus/logger'
+
+import GithubActivity from '../models/github/activity'
+import GithubActivityCache from '../models/github/cache'
+import Recurse from '../helpers/recurse'
+
+let logger = Logger('jobs/update-cache')
+
+function saveActivityCounts(item, index, next) {
+  let date = Object.keys(item)[0]
+
+  GithubActivityCache.update(
+    { date: date },
+    { date: date, total: item[date] },
+    { upsert: true, setDefaultsOnInsert: true },
+    (err) => {
+      if (err) {
+        logger.error(`GithubActivityCache.update error: ${err.message}`)
+        return next(err)
+      }
+
+      next()
+    })
+}
+
+function updateCache(done) {
+  GithubActivity.find({}, (err, found) => {
+    if (err) return done(err)
+
+    let dateMap = {}
+
+    found.forEach((event) => {
+      let date = new Date(event.created).toDateString()
+
+      if (dateMap.hasOwnProperty(date)) dateMap[date] += parseInt(event.count)
+      else dateMap[date] = parseInt(event.count)
+    })
+
+    Recurse(dateMap, saveActivityCounts, done)
+  })
+}
+
+export default function updateActivityCache() {
+  logger.info('Running job...')
+
+  updateCache((err) => {
+    if (err) return logger.error(`error: ${err.message}`)
+    logger.info('...completed')
+  })
+}
