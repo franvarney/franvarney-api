93f3538fe566376c8c4f6a84b591421453eac06b
Fran Varney <fran@modulus.io>
2016-01-12 22:19:01 -0500

    Add get all github activities handler

---
+++ server/handlers/github.js
@@ -0,0 +1,35 @@
+import {badRequest} from 'boom'
+import Logger from '@modulus/logger'
+
+import GithubActivityCache from '../models/github/cache'
+
+let logger = Logger('handlers/github')
+
+export default {
+
+  /////////// Github.getAll \\\\\\\\\\
+  getAll: {
+    handler: function (request, reply) {
+      GithubActivityCache.find({}, (err, activities) => {
+        if (err) {
+          logger.error(`GithubActivityCache.find error: ${err.message}`)
+          return reply(badRequest(err.message))
+        }
+
+        logger.debug(`GithubActivityCache.find found ${JSON.stringify(activities)}`)
+
+        activities = activities.sort((a, b) => {
+          a = new Date(a.date)
+          b = new Date(b.date)
+
+          if(a < b) return -1
+          if(a > b) return 1
+
+          return 0
+        })
+
+        reply(activities)
+      })
+    }
+  }
+}
