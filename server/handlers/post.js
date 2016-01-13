999469eecdf65c2130aa667789e7132e0bc8d74d
Fran Varney <fran@modulus.io>
2016-01-12 22:45:20 -0500

    Add create post handler

---
+++ server/handlers/post.js
@@ -0,0 +1,46 @@
+import {badRequest, notFound} from 'boom'
+import Joi from 'joi'
+import Logger from '@modulus/logger'
+import Slug from 'slug'
+
+import Post from '../models/blog/post'
+
+let logger = Logger('handlers/post')
+
+export default {
+
+  /////////// Post.create \\\\\\\\\\
+  create: {
+    validate: {
+      payload: {
+        title: Joi.string().required(),
+        image: Joi.string(),
+        caption: Joi.string(),
+        content: Joi.string().required(),
+        tags: Joi.array()
+      }
+    },
+    handler: function (request, reply) {
+      let {caption, content, image, tags, title} = request.payload
+
+      let newPost =  {
+        title: title,
+        slug: Slug(title, { lower: true }),
+        image: image,
+        caption: caption,
+        content: content,
+        tags: tags
+      }
+
+      new Post(newPost).save((err, created) => {
+        if (err) {
+          logger.error(`Post.create error: ${err.message}`)
+          return reply(badRequest(err.message))
+        }
+
+        logger.debug(`Post.create saved ${JSON.stringify(created)}`)
+        reply(created)
+      })
+    }
+  }
+}
