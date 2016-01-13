063421e7ffe7f2b7b6d713a48f66fc8272c90145
Fran Varney <fran@modulus.io>
2016-01-12 22:45:01 -0500

    Add pre save function

--- server/models/blog/post.js
+++ server/models/blog/post.js
@@ -7,9 +7,19 @@ let Post = new Schema({
   image: { type: String },
   caption: { type: String },
   content: { type: String, required: true },
-  tags: { type: Array, index: true }
+  tags: { type: Array, index: true, default: [] }
 })
 
 Post.plugin(Timestamps)
+
+Post.pre('save', function (next) {
+  Mongoose.models['Post'].find({
+    slug: this.slug
+  }, (err, results) => {
+    if (err) return next(new Error(err.message))
+    if (results.length > 0) return next(new Error('Post already exists'))
+    next()
+  })
+})
 
 export default Mongoose.model('Post', Post)
