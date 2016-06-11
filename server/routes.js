fca6904931edfbb2f415b71efac22eb6e2aad084
Fran Varney <fvarney@progress.com>
2016-06-11 13:00:48 -0700

    Add some config options

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,5 @@
 const Job = require('./handlers/job')
+const JobSchema = require('./schemas/job')
 const Home = require('./handlers/home')
 const Github = require('./handlers/github')
 const Ping = require('./handlers/ping')
@@ -6,25 +7,129 @@ const Place = require('./handlers/place')
 const Post = require('./handlers/post')
 
 module.exports = [
-  { method: 'GET', path: '/', config: { auth: false, handler: Home } },
+  {
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: false,
+      handler: Home
+    }
+  },
 
-  { method: 'GET', path: '/ping', config: { auth: false, handler: Ping } },
+  {
+    method: 'GET',
+    path: '/ping',
+    config: {
+      auth: false,
+      handler: Ping
+    }
+  },
 
-  { method: 'POST', path: '/jobs', config: Job.create },
-  { method: 'GET', path: '/jobs/{id}', config: Job.get },
-  { method: 'GET', path: '/jobs', config: Job.getAll },
-  { method: 'DELETE', path: '/jobs/{id}', config: Job.remove },
-  { method: 'PUT', path: '/jobs/{id}', config: Job.update },
+  {
+    method: 'GET',
+    path: '/github/activities',
+    config: {
+      auth: false,
+      handler: Github.getAll
+    }
+  },
 
-  { method: 'GET', path: '/github/activities', config: Github.getAll },
+  {
+    method: 'POST',
+    path: '/jobs',
+    config: {
+      validate: {
+        payload: JobSchema.create,
+      },
+      handler: Job.create
+    }
+  },
 
-  { method: 'POST', path: '/places', config: Place.create },
-  { method: 'GET', path: '/places', config: Place.getAll },
-  { method: 'GET', path: '/places/search', config: Place.search },
+  {
+    method: 'GET',
+    path: '/jobs/{id}',
+    config: {
+      auth: false,
+      handler: Job.get
+    }
+  },
 
-  { method: 'POST', path: '/posts', config: Post.create },
-  { method: 'GET', path: '/posts/{slug}', config: Post.get },
-  { method: 'GET', path: '/posts', config: Post.getAll },
-  { method: 'DELETE', path: '/posts/{slug}', config: Post.remove },
-  { method: 'PUT', path: '/posts/{slug}', config: Post.update }
+  {
+    method: 'GET',
+    path: '/jobs',
+    config: {
+      auth: false,
+      validate: {
+        query: JobSchema.getAllQuery
+      },
+      handler: Job.getAll
+    }
+  },
+
+  {
+    method: 'DELETE',
+    path: '/jobs/{id}',
+    config: {
+      handler: Job.remove
+    }
+  },
+
+  {
+    method: 'PUT',
+    path: '/jobs/{id}',
+    config: {
+      validate: {
+        payload: JobSchema.update
+      },
+      handler: Job.update
+    }
+  },
+
+  {
+    method: 'POST',
+    path: '/places',
+    config: Place.create
+  },
+
+  {
+    method: 'GET',
+    path: '/places',
+    config: Place.getAll
+  },
+
+  {
+    method: 'GET',
+    path: '/places/search',
+    config: Place.search
+  },
+
+  {
+    method: 'POST',
+    path: '/posts',
+    config: Post.create
+  },
+
+  {
+    method: 'GET',
+    path: '/posts/{slug}',
+    config: Post.get
+  },
+
+  {
+    method: 'GET',
+    path: '/posts',
+    config: Post.getAll
+  },
+
+  {
+    method: 'DELETE',
+    path: '/posts/{slug}',
+    config: Post.remove
+  },
+
+  {
+    method: 'PUT',
+    path: '/posts/{slug}',
+    config: Post.update
+  }
 ]
