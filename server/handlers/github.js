411e6bd89535e4296ddf10752fb424e259baef2f
Fran Varney <fvarney@progress.com>
2016-06-11 12:59:41 -0700

    Move config options to routes file

--- server/handlers/github.js
+++ server/handlers/github.js
@@ -3,23 +3,20 @@ const Logger = require('franston')('handlers/github')
 
 const GithubActivityCache = require('../models/github/activity-cache')
 
-exports.getAll = {
-  auth: false,
-  handler: function (request, reply) {
-    GithubActivityCache.find({}, (err, activities) => {
-      if (err) return Logger.error(err), reply(Boom.badRequest(err.message))
+exports.getAll = function (request, reply) {
+  GithubActivityCache.find({}, (err, activities) => {
+    if (err) return Logger.error(err), reply(Boom.badRequest(err.message))
 
-      activities = activities.sort((a, b) => {
-        a = new Date(a.date)
-        b = new Date(b.date)
+    activities = activities.sort((a, b) => {
+      a = new Date(a.date)
+      b = new Date(b.date)
 
-        if (a < b) return -1
-        if (a > b) return 1
+      if (a < b) return -1
+      if (a > b) return 1
 
-        return 0
-      })
-
-      return Logger.debug(activities), reply(activities)
+      return 0
     })
-  }
+
+    return Logger.debug(activities), reply(activities)
+  })
 }
