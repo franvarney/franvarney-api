598662ee4fa417eb66d02dfbb7aaf43f76bfcf27
Fran Varney <fvarney@progress.com>
2016-06-16 08:46:57 -0400

    Add place schemas

--- server/schemas/place.js
+++ server/schemas/place.js
@@ -18,3 +18,20 @@ exports.create = Joi.object({
 exports.getAllQuery = Joi.object({
   visitors: Joi.boolean()
 }).options({ stripUnknown: true })
+
+exports.response = Joi.array().items(Joi.object({
+  location: Joi.object({
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required()
+  }).rename('lat', 'latitude')
+    .rename('lng', 'longitude'),
+  name: Joi.string().required(),
+  placeId: Joi.string().required()
+}).rename('place_id', 'placeId')
+  .options({ stripUnknown: true }))
+
+exports.searchQuery = Joi.object({
+  location: Joi.string().required(),
+  keyword: Joi.string()
+})
+
