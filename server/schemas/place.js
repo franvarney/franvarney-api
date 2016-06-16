e2f32b8d45eb872a4f1aa5149586a3b9fcb47dd8
Fran Varney <fvarney@progress.com>
2016-06-16 08:35:53 -0400

    Add create place schema

--- server/schemas/place.js
+++ server/schemas/place.js
@@ -1,5 +1,20 @@
 const Joi = require('joi')
 
+exports.create = Joi.object({
+  location: Joi.object({
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required()
+  }),
+  place: {
+    id: Joi.string(),
+    name: Joi.string()
+  },
+  visitor: {
+    message: Joi.string().required(),
+    name: Joi.string().allow("", null)
+  }
+}).options({ stripUnknown: true })
+
 exports.getAllQuery = Joi.object({
   visitors: Joi.boolean()
 }).options({ stripUnknown: true })
