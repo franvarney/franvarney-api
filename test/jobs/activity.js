41f7849f4f23330fa3a03171d1243ced2594dcd1
Fran Varney <fvarney@progress.com>
2016-06-21 09:13:53 -0400

    Add more test files

---
+++ test/jobs/activity.js
@@ -0,0 +1,7 @@
+const {expect} = require('code') // eslint-disable-line
+const Lab = require('lab')
+
+const lab = exports.lab = Lab.script()
+const {describe, it} = lab // eslint-disable-line
+
+const Activity = require('../../server/jobs/get-github-activity') // eslint-disable-line
---
+++ test/jobs/cache.js
@@ -0,0 +1,7 @@
+const {expect} = require('code') // eslint-disable-line
+const Lab = require('lab')
+
+const lab = exports.lab = Lab.script()
+const {describe, it} = lab // eslint-disable-line
+
+const Cache = require('../../server/jobs/update-activity-cache') // eslint-disable-line
