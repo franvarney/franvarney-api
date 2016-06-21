2b5595960c63135bc075e14275d398e9f3c07e92
Fran Varney <fvarney@progress.com>
2016-06-21 09:06:04 -0400

    Add test files

---
+++ test/handlers/github.js
@@ -0,0 +1,7 @@
+const {expect} = require('code') // eslint-disable-line
+const Lab = require('lab')
+
+const lab = exports.lab = Lab.script()
+const {describe, it} = lab // eslint-disable-line
+
+const GitHub = require('../../server/handlers/github') // eslint-disable-line
